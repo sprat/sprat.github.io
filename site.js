f11ab648adf627e1931d14add9e0bd8bb04f71a1
Sylvain Prat <sylvain.prat@achird.net>
2016-02-22 21:43:47 +0100

    CV work in progress

---
+++ site.js
@@ -0,0 +1,11 @@
+(function() {
+    'use strict';
+
+    var emailLink = document.querySelector('.email a'),
+        encodedEmail = emailLink.innerHTML,
+        email = atob(encodedEmail);
+
+    emailLink.innerHTML = email;
+    emailLink.setAttribute('href', 'mailto:' + email);
+    console.log(emailLink);
+}());
