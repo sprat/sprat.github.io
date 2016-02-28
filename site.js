4426d4e0c1739f7c8e0a1ab825be8af739b38064
Sylvain Prat <sylvain.prat@gmail.com>
2016-02-28 15:36:20 +0100

    Added portfolio images. We need a plugin now

--- site.js
+++ site.js
@@ -1,10 +1,14 @@
 (function() {
     'use strict';
 
-    var emailLink = document.querySelector('.email a'),
-        encodedEmail = emailLink.innerHTML,
-        email = atob(encodedEmail);
+    function initEmailLinks() {
+        var emailLink = document.querySelector('.email a'),
+            encodedEmail = emailLink.innerHTML,
+            email = atob(encodedEmail);
 
-    emailLink.innerHTML = email;
-    emailLink.setAttribute('href', 'mailto:' + email);
+        emailLink.innerHTML = email;
+        emailLink.setAttribute('href', 'mailto:' + email);
+    }
+
+    initEmailLinks();
 }());
