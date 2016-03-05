c99e1091410f5c182b2057d87fa42a628ccf1321
Sylvain Prat <sylvain.prat@achird.net>
2016-03-05 14:55:06 +0100

    Rich snippets + encode the email differently

--- site.js
+++ site.js
@@ -5,10 +5,16 @@
         $('.main-menu').sticky();
     }
 
+    function rot13(s) {
+        return s.replace(/[a-zA-Z]/g, function (c) {
+            return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
+        });
+    }
+
     function initEmailLinks() {
-        var emailLink = document.querySelector('a.email'),
+        var emailLink = document.querySelector('a[itemprop="email"]'),
             encodedEmail = emailLink.innerHTML,
-            email = atob(encodedEmail);
+            email = rot13(encodedEmail);
 
         emailLink.innerHTML = email;
         emailLink.setAttribute('href', 'mailto:' + email);
