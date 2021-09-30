0502d7c230414fbac4f2a33603d767ac63f69e20
Sylvain Prat <sylvain.prat@achird.net>
2021-09-30 21:26:12 +0200

    Remove email protection, cloudflare implements a protection already

--- public/site.js
+++ public/site.js
@@ -5,21 +5,6 @@
         $('.main-menu').sticky();
     }
 
-    function rot13(s) {
-        return s.replace(/[a-zA-Z]/g, function (c) {
-            return String.fromCharCode((c <= "Z" ? 90 : 122) >= (c = c.charCodeAt(0) + 13) ? c : c - 26);
-        });
-    }
-
-    function initEmailLinks() {
-        var emailLink = document.querySelector('a.email'),
-            encodedEmail = emailLink.innerHTML,
-            email = rot13(encodedEmail);
-
-        emailLink.innerHTML = email;
-        emailLink.setAttribute('href', 'mailto:' + email);
-    }
-
     function initDiaporamas() {
         $('.diaporama').fotorama({
             width: '100%',
@@ -30,6 +15,5 @@
     }
 
     initMenu();
-    initEmailLinks();
     initDiaporamas();
 }());
